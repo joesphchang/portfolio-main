cd08a7188d995aec912310171ee12fdd02c2777c
joesphchang <joesphschang@gmail.com>
2022-01-25 23:35:44 -0500

    Updated languages and tools

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,7 +19,7 @@ function Home(props) {
 						Over 4 years of experience in project-management, creative direction
 						and effective communcation with demonstrated history working
 						along-side professional teams to deliver optimized product
-						experiences. Passionate about transforming big ideas into tangible
+						experiences. Passionate about transforming ideas into tangible
 						innovations.
 					</p>
 				</div>
@@ -29,15 +29,6 @@ function Home(props) {
 					</h3>
 					<p align='left'>
 						{' '}
-						<a href='https://getbootstrap.com' target='_blank' rel='noreferrer'>
-							{' '}
-							<img
-								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg'
-								alt='bootstrap'
-								width='40'
-								height='40'
-							/>{' '}
-						</a>{' '}
 						<a
 							href='https://www.w3schools.com/css/'
 							target='_blank'
@@ -46,18 +37,6 @@ function Home(props) {
 							<img
 								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg'
 								alt='css3'
-								width='40'
-								height='40'
-							/>{' '}
-						</a>{' '}
-						<a
-							href='https://www.djangoproject.com/'
-							target='_blank'
-							rel='noreferrer'>
-							{' '}
-							<img
-								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/django/django-original.svg'
-								alt='django'
 								width='40'
 								height='40'
 							/>{' '}
@@ -80,23 +59,29 @@ function Home(props) {
 								height='40'
 							/>{' '}
 						</a>{' '}
+						<a href='https://git-scm.com/' target='_blank' rel='noreferrer'>
+							{' '}
+							<img
+								src='https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg'
+								alt='git'
+								width='40'
+								height='40'
+							/>{' '}
+						</a>{' '}
+						<a href='https://heroku.com' target='_blank' rel='noreferrer'>
+							{' '}
+							<img
+								src='https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg'
+								alt='heroku'
+								width='40'
+								height='40'
+							/>{' '}
+						</a>{' '}
 						<a href='https://www.w3.org/html/' target='_blank' rel='noreferrer'>
 							{' '}
 							<img
 								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg'
 								alt='html5'
-								width='40'
-								height='40'
-							/>{' '}
-						</a>{' '}
-						<a
-							href='https://www.adobe.com/in/products/illustrator.html'
-							target='_blank'
-							rel='noreferrer'>
-							{' '}
-							<img
-								src='https://www.vectorlogo.zone/logos/adobe_illustrator/adobe_illustrator-icon.svg'
-								alt='illustrator'
 								width='40'
 								height='40'
 							/>{' '}
@@ -109,6 +94,16 @@ function Home(props) {
 							<img
 								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg'
 								alt='javascript'
+								width='40'
+								height='40'
+							/>{' '}
+						</a>{' '}
+						{' '}
+						<a href='https://mochajs.org' target='_blank' rel='noreferrer'>
+							{' '}
+							<img
+								src='https://www.vectorlogo.zone/logos/mochajs/mochajs-icon.svg'
+								alt='mocha'
 								width='40'
 								height='40'
 							/>{' '}
@@ -155,11 +150,11 @@ function Home(props) {
 								height='40'
 							/>{' '}
 						</a>{' '}
-						<a href='https://www.python.org' target='_blank' rel='noreferrer'>
+						<a href='https://postman.com' target='_blank' rel='noreferrer'>
 							{' '}
 							<img
-								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg'
-								alt='python'
+								src='https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg'
+								alt='postman'
 								width='40'
 								height='40'
 							/>{' '}
@@ -169,45 +164,6 @@ function Home(props) {
 							<img
 								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg'
 								alt='react'
-								width='40'
-								height='40'
-							/>{' '}
-						</a>{' '}
-						<a href='https://reactnative.dev/' target='_blank' rel='noreferrer'>
-							{' '}
-							<img
-								src='https://reactnative.dev/img/header_logo.svg'
-								alt='reactnative'
-								width='40'
-								height='40'
-							/>{' '}
-						</a>{' '}
-						<a href='https://sass-lang.com' target='_blank' rel='noreferrer'>
-							{' '}
-							<img
-								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg'
-								alt='sass'
-								width='40'
-								height='40'
-							/>{' '}
-						</a>{' '}
-						<a href='https://spring.io/' target='_blank' rel='noreferrer'>
-							{' '}
-							<img
-								src='https://www.vectorlogo.zone/logos/springio/springio-icon.svg'
-								alt='spring'
-								width='40'
-								height='40'
-							/>{' '}
-						</a>{' '}
-						<a
-							href='https://www.typescriptlang.org/'
-							target='_blank'
-							rel='noreferrer'>
-							{' '}
-							<img
-								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg'
-								alt='typescript'
 								width='40'
 								height='40'
 							/>{' '}
